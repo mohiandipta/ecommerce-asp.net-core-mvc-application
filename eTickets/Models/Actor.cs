30b7ed8c4e386196325161f7bdbafe09d16f15a3
mohiandipta <mohianmostofa@gmail.com>
2021-10-02 11:57:51 +0600

    actor scheema, Actor_Movie and Movie relationships added

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -17,5 +17,9 @@ namespace eTickets.Models
         public string FullName { get; set; }
 
         public string Bio { get; set; }
+
+
+        // Relationships
+        public List<Actor_Movie> Actor_Movies { get; set; }
     }
 }
--- eTickets/Models/Movie.cs
+++ eTickets/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -22,5 +23,23 @@ namespace eTickets.Models
         public DateTime EndDate { get; set; }
 
         public MovieCategory MovieCategory { get; set; }
+
+
+        // Relationships
+        public List<Actor_Movie> Actor_Movies { get; set; }
+
+
+        // design has defined as [ Cinemas ---> Movies <--- Producers ]
+
+        // Cinema 
+        public int CinemaId { get; set; }
+        [ForeignKey("CinemaId")]
+        public Cinema Cinema { get; set; }
+
+
+        // Producer
+        public int ProducerId { get; set; }
+        [ForeignKey("ProducerId")]
+        public Producer Producer { get; set; }
     }
 }
