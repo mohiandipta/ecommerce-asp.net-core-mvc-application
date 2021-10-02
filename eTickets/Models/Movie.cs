ee7c2808208d9cded976cb750d3ca29cffe99325
mohiandipta <mohianmostofa@gmail.com>
2021-10-02 10:43:14 +0600

    adding movie and movieCategory model, using enum for movieCategory dataType

---
+++ eTickets/Models/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public MovieCategory MovieCategory { get; set; }
+    }
+}
